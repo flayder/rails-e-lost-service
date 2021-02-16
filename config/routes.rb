f37e332fbc9e9d41e6d85a370354cc981fe34d95
Jorge Barra <jorge.barra1412@gmail.com>
2021-02-16 16:49:10 -0300

    resource authentication added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
-  resources :products
-  resources :categories
-
   root to: "home#index"
+  
+  authenticate :admin do
+    resources :products
+    resources :categories  
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
