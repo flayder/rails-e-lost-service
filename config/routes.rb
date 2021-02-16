3d839baafc0d16f493fcaf95d858ab31731f0d4a
Jorge Barra <jorge.barra1412@gmail.com>
2021-02-16 16:40:36 -0300

    devise Admin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :products
   resources :categories
+
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
